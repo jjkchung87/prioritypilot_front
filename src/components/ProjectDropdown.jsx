51f3994a361d34a0f914f2fa2e8b89ca81be8ef0
IrinaJ <irina.ul87@gmail.com>
2023-10-14 18:05:34 -0400

    dropdown for users projects added

--- src/components/ProjectDropdown.jsx
+++ src/components/ProjectDropdown.jsx
@@ -1,16 +1,12 @@
 import { useState, useContext } from "react"
 import { UserContext } from "../context/UserContext"
-import { useNavigate } from "react-router-dom"
-
 
 function ProjectDropdown() {
   const { currentUser } = useContext(UserContext)
   const [project, setProject] = useState('Select Project')
-
   const handleSelectProject = (e) => {
     setProject(e.target.value)
   }
-  console.log(currentUser.projects)
   return (
     <div className="dropdown wrapper">
       <select onChange={handleSelectProject} className="select-project">
